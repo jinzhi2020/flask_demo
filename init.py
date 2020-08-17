82434de00b34e0390b2b9bb84c034e23a3b04571
JinZhiSu <happy@hacking.icu>
2020-08-17 22:35:28 +0800

    load config file

--- init.py
+++ init.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from app.http.v1 import bp as v1_bp
 from app.http.v2 import bp as v2_bp
+from app.http.v1.models.products import db
 
 
 def create_app(app_name):
     app = Flask(app_name)
+    _load_configs(app)
     _register_blueprint(app)
+    _init_database(app)
     return app
+
+
+def _load_configs(app):
+    app.config.from_object('app.configs.secure')
+    app.config.from_object('app.configs.setting')
+
+
+def _init_database(app):
+    db.init_app(app)
 
 
 def _register_blueprint(app):
